727254a8f631a618fd1417aec7be3f4732e1531c
Eiranpreet Khangura <eiran@Eiranpreets-MacBook-Pro.local>
2020-10-26 16:18:57 +0000

    created setup.py

---
+++ Pluralsight/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Flask-REST-Api-Example',
+    version='0.1.0',
+    description='A project to help understand REST apis using flask',
+    author='Eiran Khangura',
+    author_email='eiranpreet.k@gmail.com',
+    install_requires=[
+        'cffi==1.14.3',
+        'click==7.1.2',
+        'cryptography==2.9.2',
+        'Flask==1.1.2',
+        'Flask-SQLAlchemy==2.4.4',
+        'itsdangerous==1.1.0',
+        'Jinja2==2.11.2',
+        'MarkupSafe==1.1.1',
+        'pycparser==2.20',
+        'PyJWT==1.7.1',
+        'six==1.15.0',
+        'SQLAlchemy==1.3.20',
+        'Werkzeug==1.0.1'
+    ]
+)
